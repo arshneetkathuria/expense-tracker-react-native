c1b7b1579a1f4156ed306f79677343145674df2e
Arshneet Kathuria <arntkr13@gmail.com>
2022-10-13 13:42:51 +0530

    final commit

--- code/04-adding-dummy-expense-data/components/ExpensesOutput/ExpensesList.js
+++
@@ -1,7 +0,0 @@
-import { FlatList } from 'react-native';
-
-function ExpensesList() {
-  return <FlatList />;
-}
-
-export default ExpensesList;
--- code/08-formatting-dates/screens/RecentExpenses.js
+++
@@ -1,7 +0,0 @@
-import ExpensesOutput from '../components/ExpensesOutput/ExpensesOutput';
-
-function RecentExpenses() {
-  return <ExpensesOutput expensesPeriod="Last 7 Days" />;
-}
-
-export default RecentExpenses;
